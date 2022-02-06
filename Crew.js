97e0e14a46a6971946aab55e8dd7b868f603c330
Sergio Arenas <sergioarenas025@gmail.com>
2022-02-06 09:48:42 -0500

    Already done for mobile, is need to be done for pc and tablet(I'm working on that)

---
+++ Crew.js
@@ -0,0 +1,35 @@
+const button_menu= document.getElementById("menu")
+const button_close=document.getElementById("close")
+const modal = document.querySelector(".modal")
+
+
+button_close.addEventListener("click",()=>{
+    modal.classList.remove("visible")
+    modal.classList.add("hiden")})
+
+
+button_menu.addEventListener("click",()=>{
+    modal.classList.remove("hiden")
+    modal.classList.add("visible")
+})
+
+const change_home= document.getElementById("home")
+const change_destination= document.getElementById("destination")
+const change_crew= document.getElementById("crew")
+const change_technology= document.getElementById("technology")
+
+change_home.addEventListener("click",()=>{
+    window.location.assign("/Home.html")
+})
+
+change_destination.addEventListener("click",()=>{
+    window.location.assign("/Destination.html")
+})
+
+change_crew.addEventListener("click",()=>{
+    window.location.assign("/Crew.html")
+})
+
+change_technology.addEventListener("click",()=>{
+    window.location.assign("/Technology.html")
+})
